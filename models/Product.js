e4a57a8f4ff81d852b997828cd75774046924a99
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-18 14:05:40 -0400

    defining columns for category table

--- models/Product.js
+++ models/Product.js
@@ -8,16 +8,48 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+	{
+		// define columns
+		id: {
+			type: dataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		product_name: {
+			type: dataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: dataTypes.DECIMAL,
+			allowNull: false,
+			validate: {
+				isDecimal: true,
+			},
+		},
+		stock: {
+			type: dataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 10,
+			validate: {
+				isNumeric: true,
+			},
+		},
+		category_id: {
+			type: dataTypes.INTEGER,
+			references: {
+				model: 'category',
+				key: 'id',
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'product',
+	}
 );
 
 module.exports = Product;
