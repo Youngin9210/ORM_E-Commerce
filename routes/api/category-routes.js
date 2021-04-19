a3c1b4bf85c278f1a816702d7c08345136728f8b
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-19 20:25:05 +0000

    successfully updating (PUT) category by ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,6 +42,16 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    return !categoryData[0]
+      ? res.status(404).json({ message: "No category found with that ID" })
+      : res.status(200).json(categoryData);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
