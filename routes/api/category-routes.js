3a5c521f6719bd26edfcd786febc49eb1f45eab2
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-19 20:15:54 +0000

    successfully creating (POST) new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,6 +32,12 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 router.put("/:id", async (req, res) => {
