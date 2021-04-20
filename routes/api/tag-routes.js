295439cac1e4e0c5bed2d5e42daa12af8a3b29bf
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-20 17:04:57 +0000

    successfully updating tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,6 +39,16 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    return !tagData[0]
+      ? res.status(404).json({ message: "No tag found with that ID" })
+      : res.status(200).json(tagData);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
