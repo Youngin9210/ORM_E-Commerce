abb7d3d7dc3a8be4860e27d743d3326c3ba529be
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-22 22:19:17 +0000

    commenting

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
-
-const Sequelize = require('sequelize');
-
+// including dotenv to hide credentials in a .env file
+require("dotenv").config();
+// including sequelize to interact with mysql database
+const Sequelize = require("sequelize");
+// setting connection requirements from .env file
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "localhost",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
     });
-
+// exporting sequelize to be used in other files
 module.exports = sequelize;
